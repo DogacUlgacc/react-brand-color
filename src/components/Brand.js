5e95c022a4be61539e8b616ba041a7806faff57b
DogacUlgacc <ulgac1907@gmail.com>
2023-10-04 15:33:11 +0300

    brand colors copied

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { getContrastYIQ } from "./helper";
-export default function Brand(brand) {
+import { FaCopy } from "react-icons/fa";
+
+export default function Brand({ brand }) {
+    const [copiedColor, setCopiedColor] = useState(null);
+    const [copyIcon, setCopyIcon] = useState(false);
+
+    const copyToClipboard = (text) => {
+        navigator.clipboard.writeText(text).then(() => {
+            setCopiedColor(text);
+            setTimeout(() => {
+                setCopiedColor(null);
+            }, 2000);
+        });
+    };
+
     return (
         <div className="brand">
-            <h4>{brand.brand.title}</h4>
+            <h4>{brand.title}</h4>
             <h5>
-                {brand.brand.colors.map((color, index) => (
+                {brand.colors.map((color, index) => (
                     <span
+                        onMouseEnter={() => setCopyIcon(true)}
+                        onMouseLeave={() => setCopyIcon(false)}
+                        className="brand-colors"
+                        onClick={() => copyToClipboard(color)}
                         style={{
                             backgroundColor: `#${color}`,
                             color: getContrastYIQ(color),
+                            cursor: "pointer",
                         }}
-                        className="brand-colors"
                         key={index}
                     >
-                        {color}
+                        {copyIcon && (
+                            <>
+                                <FaCopy style={{ marginRight: "5px" }} /> #
+                                {color}
+                            </>
+                        )}
                     </span>
                 ))}
             </h5>
+            {copiedColor && (
+                <h4> Color code copied successfully: #{copiedColor}</h4>
+            )}
         </div>
     );
 }
